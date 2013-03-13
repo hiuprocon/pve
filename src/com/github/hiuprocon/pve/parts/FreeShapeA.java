09812a96f1fef8bf239e1b38ac1b9781cc10160e
Kenji Saito <ksaito@do-johodai.ac.jp>
2013-03-13 10:17:21 +0900

    move to a314.

---
+++ src/com/github/hiuprocon/pve/parts/FreeShapeA.java
@@ -0,0 +1,35 @@
+package com.github.hiuprocon.pve.parts;
+
+import com.bulletphysics.collision.shapes.*;
+import com.bulletphysics.dynamics.*;
+import com.bulletphysics.linearmath.MotionState;
+import com.bulletphysics.linearmath.Transform;
+import com.github.hiuprocon.pve.core.*;
+import javax.vecmath.*;
+import jp.sourceforge.acerola3d.a3.*;
+
+public class FreeShapeA extends PVEPart {
+    public FreeShapeA(PartType pt,A3Object a) {
+        super(tp,new Vector3d(0,0,0),new Vector3d());
+    }
+
+    public A3Object makeA3Object(Object...args) throws Exception {
+        VRML vrml = new VRML("x-rzip:x-res:///res/stk_racetrack.a3!/racetrack.wrl");
+        vrml.setUserData("地面2");
+        return vrml;
+    }
+    public MotionState makeMotionState(Vector3d l,Vector3d r) {
+        Transform transform = new Transform();
+        transform.setIdentity();
+        transform.origin.set((float)l.x,(float)l.y,(float)l.z);
+        transform.setRotation(new Quat4f(Util.euler2quat(r)));
+        return new A3MotionState(a3,transform);
+    }
+    //地面用の剛体を作る
+    public RigidBody makeRigidBody(Object...args) {
+        CollisionShape groundShape = Util.makeBvhTriangleMeshShape(a3.getNode());
+        RigidBodyConstructionInfo cInfo = new RigidBodyConstructionInfo(0.0f, motionState, groundShape, new Vector3f());
+        RigidBody body = new RigidBody(cInfo);
+        return body;
+    }
+}
