979a9d19e957d3dce0f7cbb4f87e4fb2f2a7f5d6
Kenji Saito <ksaito@do-johodai.ac.jp>
2014-03-21 00:46:25 +0900

    avatar

---
+++ src/com/github/hiuprocon/pve/obj/Avatar.java
@@ -0,0 +1,23 @@
+package com.github.hiuprocon.pve.obj;
+
+import com.github.hiuprocon.pve.core.*;
+import javax.vecmath.*;
+import jp.sourceforge.acerola3d.a3.Util;
+
+public class Avatar extends SphereObj {
+    public Avatar(String a3url) {
+        super(Type.DYNAMIC,1.0,1.0,a3url);
+        PVEPart mp = getMainPart();
+        mp.setAngularFactor(0);
+    }
+
+    public void setForce(double zengo, double sayu) {
+        PVEPart mp = getMainPart();
+        Quat4d q = mp.getQuat();
+        Vector3d front = Util.trans(q,new Vector3d(0,0,1));
+        front.scale(zengo);
+        Vector3d angVel = new Vector3d(0,sayu,0);
+        mp.applyCentralForce(front);
+        mp.setAngularVelocity(angVel);
+    }
+}
