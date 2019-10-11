d56b40f77f1e88acf50180425f5160dd5496b88f
Kenji Saito <ksaito@a314.do-johodai.ac.jp>
2019-10-11 13:32:52 +0900

    samples4/TestFreeObj.javaにFreeObjA,B,C,Dを説明するコメントを付けた。

--- samples4/TestFreeObj.java
+++ samples4/TestFreeObj.java
@@ -4,6 +4,25 @@ import com.github.hiuprocon.pve.obj.*;
 import jp.sourceforge.acerola3d.a3.*;
 import javax.vecmath.*;
 
+/*
+自由な形を作るためのPVEObjectは
+FreeObjA, FreeObjB, FreeObjC, FreeObjDの
+4種類あります。
+
+* FreeObjA: は本当に自由な形が作れる。ただ，
+  衝突判定のコストが高い。
+* FreeObjB: 指定された形を取り囲む最小の凸包。
+  穴のあいてる形や，くぼみのある形は作れないが
+  衝突判定が低コストでできるのでお勧め。
+* FreeObjC: 生成後に形が変えられるjbulletの
+  GImpactMeshShapeで実装されているのだが，
+  形を変えるAPIが無いので意味無し。
+* FreeObjD: jbulletのCollisionShapeを元に生成
+  するPVEPart。特にCollisionShapeのサブクラスの
+  CompoundShapeを使って形を組合せて新しい形を
+  作る時に有用。
+*/
+
 class Test {
     public static void main(String args[]) {
         //仮想環境生成
